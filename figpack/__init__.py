09803c2e4dc67f59a0d3427714a3dacf0bafcff3
Jeremy Magland <jmagland@flatironinstitute.org>
2025-08-15 17:06:29 -0400

    incr version

--- figpack/__init__.py
+++ figpack/__init__.py
@@ -2,7 +2,7 @@
 figpack - A Python package for creating shareable, interactive visualizations in the browser
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.4"
 __author__ = "Jeremy Magland"
 __email__ = "jmagland@flatironinstitute.org"
 
