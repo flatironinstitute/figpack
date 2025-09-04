0b0fa95ecfef64d749abe271894fa476bd82df0f
Jeremy Magland <jmagland@flatironinstitute.org>
2025-09-04 19:16:00 -0400

    incr version

--- figpack/__init__.py
+++ figpack/__init__.py
@@ -2,7 +2,7 @@
 figpack - A Python package for creating shareable, interactive visualizations in the browser
 """
 
-__version__ = "0.2.12"
+__version__ = "0.2.13"
 
 from .cli import view_figure
 from .core import FigpackView, FigpackExtension, ExtensionRegistry, ExtensionView
