e19f76257c64638c52aad548bfd820fc642506d4
Jeremy Magland <jmagland@flatironinstitute.org>
2025-09-09 19:47:39 -0400

    figpack spike sorting fixes

--- extension_packages/figpack_spike_sorting/src/index.ts
+++ extension_packages/figpack_spike_sorting/src/index.ts
@@ -1,8 +1,20 @@
 import { ZarrGroup } from "./figpack-plugin-interface/ZarrTypes";
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import View from './View';
-import { FPViewContexts } from "./figpack-plugin-interface/FPPluginInterface";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import View from "./View";
+import {
+  FPViewContext,
+  FPViewContexts,
+} from "./figpack-plugin-interface/FPPluginInterface";
+import { defaultUnitSelection } from "./views/context-unit-selection";
+import {
+  UnitSelection,
+  unitSelectionReducer,
+} from "./views/context-unit-selection/UnitSelectionContext";
+import {
+  UnitMetricSelection,
+  unitMetricSelectionReducer,
+} from "./views/context-unit-metrics-selection";
 
 // Declare global types for figpack extension system
 export {};
@@ -13,15 +25,76 @@ declare global {
   }
 }
 
+const createUnitMetricSelectionContext = (): FPViewContext => {
+  let state: UnitMetricSelection = {};
+  const listeners: ((newValue: UnitMetricSelection) => void)[] = [];
+
+  const dispatch = (action: any) => {
+    state = unitMetricSelectionReducer(state, action);
+    listeners.forEach((callback) => {
+      callback(state);
+    });
+  };
+
+  const onChange = (callback: (newValue: UnitMetricSelection) => void) => {
+    listeners.push(callback);
+    return () => {
+      const idx = listeners.indexOf(callback);
+      if (idx >= 0) listeners.splice(idx, 1);
+    };
+  };
+
+  return {
+    state,
+    dispatch,
+    onChange,
+    createNew: createUnitMetricSelectionContext,
+  };
+};
+
+const createUnitSelectionContext = (): FPViewContext => {
+  let state: UnitSelection = defaultUnitSelection;
+  const listeners: ((newValue: UnitSelection) => void)[] = [];
+
+  const dispatch = (action: any) => {
+    state = unitSelectionReducer(state, action);
+    listeners.forEach((callback) => {
+      callback(state);
+    });
+  };
+
+  const onChange = (callback: (newValue: UnitSelection) => void) => {
+    listeners.push(callback);
+    return () => {
+      const idx = listeners.indexOf(callback);
+      if (idx >= 0) listeners.splice(idx, 1);
+    };
+  };
+
+  return {
+    state,
+    dispatch,
+    onChange,
+    createNew: createUnitSelectionContext,
+  };
+};
+
 // Register the figpack extension
 window.figpackExtensions = window.figpackExtensions || {};
 
-window.figpackExtensions['figpack-spike-sorting'] = {
-  render: async function(a: {container: HTMLElement, zarrGroup: ZarrGroup, width: number, height: number, onResize: (callback: (width: number, height: number) => void) => void, contexts: FPViewContexts}) {
+window.figpackExtensions["figpack-spike-sorting"] = {
+  render: async function (a: {
+    container: HTMLElement;
+    zarrGroup: ZarrGroup;
+    width: number;
+    height: number;
+    onResize: (callback: (width: number, height: number) => void) => void;
+    contexts: FPViewContexts;
+  }) {
     const { container, zarrGroup, width, height, onResize, contexts } = a;
 
-    container.innerHTML = '';
-    
+    container.innerHTML = "";
+
     try {
       const root = createRoot(container);
       root.render(
@@ -30,23 +103,33 @@ window.figpackExtensions['figpack-spike-sorting'] = {
           width,
           height,
           onResize,
-          contexts
+          contexts,
         })
       );
-      
+
       return {
         destroy: () => {
           root.unmount();
-        }
+        },
       };
     } catch (error) {
-      console.error('Error rendering Spike Sorting visualization:', error);
+      console.error("Error rendering Spike Sorting visualization:", error);
       this.renderError(container, width, height, (error as Error).message);
       return { destroy: () => {} };
     }
   },
 
-  renderError: function(container: HTMLElement, width: number, height: number, message: string): void {
+  contextCreators: [
+    { name: "unitSelection", create: createUnitSelectionContext },
+    { name: "unitMetricSelection", create: createUnitMetricSelectionContext },
+  ],
+
+  renderError: function (
+    container: HTMLElement,
+    width: number,
+    height: number,
+    message: string
+  ): void {
     container.innerHTML = `
       <div style="
         width: ${width}px; 
@@ -71,9 +154,9 @@ window.figpackExtensions['figpack-spike-sorting'] = {
     `;
   },
 
-  joinPath: function(p1: string, p2: string): string {
-    if (p1.endsWith('/')) p1 = p1.slice(0, -1);
-    if (p2.startsWith('/')) p2 = p2.slice(1);
-    return p1 + '/' + p2;
-  }
+  joinPath: function (p1: string, p2: string): string {
+    if (p1.endsWith("/")) p1 = p1.slice(0, -1);
+    if (p2.startsWith("/")) p2 = p2.slice(1);
+    return p1 + "/" + p2;
+  },
 };
