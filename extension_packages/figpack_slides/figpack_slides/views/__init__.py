f1f11f1f7de66e88f3572baad08ce0abff6faf9b
Jeremy Magland <jmagland@flatironinstitute.org>
2025-10-11 11:51:33 -0400

    move advanced markdown to slides

--- extension_packages/figpack_slides/figpack_slides/views/__init__.py
+++ extension_packages/figpack_slides/figpack_slides/views/__init__.py
@@ -5,5 +5,6 @@ figpack_slides
 from .Slides import Slides
 from .Slide import Slide, SlideText, SlideHeader, SlideFooter
 from .TitleSlide import TitleSlide
+from .Markdown import Markdown
 
 __all__ = ["Slides", "Slide", "SlideText", "SlideHeader", "SlideFooter", "TitleSlide"]
